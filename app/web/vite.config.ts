c0b979f4c51287a75110a879e57e2c741199f1c5
Hindi-Here <pechenka28f@gmail.com>
2026-04-24 12:38:54 +0300

    vite path ESM update

--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
+import { fileURLToPath } from 'node:url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
 
